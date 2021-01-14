1acc338ce299fa288829393260ec54185f2374b3
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2021-01-14 03:36:40 -0500

    hooks

---
+++ resources/js/components/pages/basic/hooks.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>My name is {{name}}</h1>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return{
+                name:'sergio'
+            }
+        },
+        created(){
+            //Llamamos datos iniciales
+        }
+    }
+</script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import firstPage from './components/pages/myFirstVuePage.vue'
+import hooks from './components/pages/basic/hooks.vue'
+
 Vue.use(Router);
 const routes =[
     {
         path:'/mynamevuerouter',
         component: firstPage
+    },
+    {
+        path:'/hooks',
+        component: hooks
     }
 ]
 
